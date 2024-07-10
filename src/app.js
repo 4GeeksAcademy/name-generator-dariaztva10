078186ec8784c1fb210d84ca6f4bbfd58812c0f7
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-07-10 09:02:18 +0000

    JS terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Deberías funcionar");
+  let pronoun = ["el", "nuestro"];
+  let adj = ["grandioso", "gran"];
+  let noun = ["informatico", "creador"];
+
+  function generateEmail() {
+    let pronounIndex = Math.floor(Math.random() * pronoun.length);
+    let adjIndex = Math.floor(Math.random() * adj.length);
+    let nounIndex = Math.floor(Math.random() * noun.length);
+
+    return (
+      pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + "@gmail.com"
+    );
+  }
+
+  let email = generateEmail();
+  console.log(email);
+
+  document.getElementById("emailContainer").innerText = email;
 };
