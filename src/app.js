a25644e8271593f025055ad95b75c602726cdc27
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-07-10 09:44:16 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,27 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const pronoun = ["el", "nuestro"];
+const adj = ["grandioso", "fantastico"];
+const noun = ["informatico", "creador"];
+const domain = ["@gmail.com", "@gmail.es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  let pronoun = ["el", "nuestro"];
-  let adj = ["grandioso", "gran"];
-  let noun = ["informatico", "creador"];
-
-  function generateEmail() {
-    let pronounIndex = Math.floor(Math.random() * pronoun.length);
-    let adjIndex = Math.floor(Math.random() * adj.length);
-    let nounIndex = Math.floor(Math.random() * noun.length);
-
-    return (
-      pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + "@gmail.com"
-    );
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < domain.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
+      }
+    }
   }
-
-  let email = generateEmail();
-  console.log(email);
-
-  document.getElementById("emailContainer").innerText = email;
-};
+}
